3224a75bf132f349a728bfea44c313094707e819
Amir <aha000111@icloud.com>
2022-06-15 17:48:24 +0100

    addition of mp3 files in music folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import {useState} from 'react';
 
 function App() {
+  const [songs, setSongs] = useState([
+    {
+      title: 'dfgdg',
+      artist: 'gagf',
+      img_src: '/public/images/tupac.png',
+      src: './public/music/tupac_AintMadAtCha'
+    },
+
+  ])
   return (
     <div className="App">
-      App
+      COMPONENETS HERE
     </div>
   );
 }
