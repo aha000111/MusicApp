707b8aa624525b1f29d6ae84722f0569b31a3faf
Amir <aha000111@icloud.com>
2022-06-16 10:22:05 +0100

    added useEffect to play music Player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import PlayerDetails from './PlayerDetails'
 import PlayerControls from './PlayerControls'
 
@@ -8,7 +8,7 @@ function Player(props) {
         <audio></audio>
         <h4>Playing now</h4>
         <PlayerDetails song={props.song} />
-        <PlayerControls song={props.song} />
+        <PlayerControls />
         <p><strong>Next up: </strong>{props.nextSong.title} by {props.nextSong.artist}</p>
     </div>
   )
